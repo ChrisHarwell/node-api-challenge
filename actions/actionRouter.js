8ae3f6fd381e45d611408d01682144d9ad435016
Chris Harwell <chrisharwell72@gmail.com>
2020-08-15 13:27:31 -0400

    actionRouter has been halfway finished, need to add validation for data otherwise it is fine

---
+++ actions/actionRouter.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const action = require("../data/helpers/actionModel.js");
+const router = express.Router();
+
+// TODO add /:id to the get params
+router.get("/", (req, res) => {
+  const { id } = req.params;
+  action
+    .get(id)
+    .then((action) => {
+      res.status(200).json(action);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({
+        message: "Error retrieving the action",
+      });
+    });
+});
+
+router.post("/", (req, res) => {
+  const actions = req.body;
+
+  action.insert(actions).then((action) => {
+    res.status(201).json(action);
+  });
+});
+
+router.put("/:id", (req, res) => {
+  action.update(req.params.id, req.body).then((action) => {
+    if (action) {
+      res.status(200).json(action);
+    } else {
+      res.status(404).json({ message: "The action could not be found" });
+    }
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  action.remove(req.params.id).then((count) => {
+      console.log(count);
+    if (count > 0) {
+      res.status(200).json({ message: "The hub has been nuked" });
+    } else {
+      res.status(404).json({ message: "The hub could not be found" });
+    }
+  });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+// const projectRouter = require("./projects/projectRouter.js");
+const actionRouter = require("./actions/actionRouter.js");
+// const helmet = require('helmet');
+const server = express();
+
+server.use(express.json());
+// server.use(helmet());
+// server.use('/api/action', projectRouter);
+server.use('/api/project', actionRouter);
+// server.use(logger);
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+//custom middleware
+
+function logger(req, res, next) {
+  console.log(`${req.method} request`);
+  next();
+}
+
+module.exports = server;
